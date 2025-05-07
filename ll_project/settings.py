f5455b01bff01d106f5a45c9d8230d2203ca41bc
jim-X-AI <jmldpp@gmail.com>
2025-05-07 02:41:15 +0100

    ready for superbase

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,11 +83,11 @@ WSGI_APPLICATION = 'll_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'r$y8yzJ8wcCTd7',  # replace this
-        'HOST': 'db.zddobwhmqoysxgzdwlzi.supabase.co',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
